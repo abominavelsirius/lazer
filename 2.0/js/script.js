92baab0b3cd74aafe3fb1cabd747dd448a6aab9f
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-04-10 23:03:23 -0300

    add

--- 2.0/js/script.js
+++ 2.0/js/script.js
@@ -1,3 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+// let hbo = document.querySelectorAll('.hbo-btn')
+// let body = document.querySelector('body')
+// console.log(body)
+
+
+
+// hbo.addEventListener('click', function() {
+//     body.style.background = 'linear-gradient(50deg, rgba(88,18,93,100) 21%, rgba(31,33,104,20) 71%);';
+
+// })
+
+
+
 // function hbo() {
 //     querySelector("body")
 // }
